68de6cf631b2f4ab0b679a1f6fc179dc665454c4
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-06-26 19:03:14 -0400

    fix logic for the test

--- src/bins.jl
+++ src/bins.jl
@@ -166,7 +166,7 @@ function Generate_bins(dist::Array{T}, Nb::Vector{Int64};verbose=false) where {T
    # Dimensão do problema
    nz, n = size(dist)
 
-   verbose || println("Generate_bins  nz=", nz, " n=",n)
+   !verbose || println("Generate_bins  nz=", nz, " n=",n)
 
    # Dicionário com os centros e pesos
    sampled = Dict{Int64,NBin_data}()
